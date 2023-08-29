8c61c6b782d816f9f131dbf1640e3498da752955
Moonlit32 <143508674+Moonlit32@users.noreply.github.com>
2023-08-29 09:42:13 -0300

    Add files via upload

---
+++ Dollar_01.py
@@ -0,0 +1,6 @@
+Dollar = float(input("Valor do dólar agora? "))
+Valor = float(input("Quantos dólares você tem? "))
+
+Reias = Dollar * Valor
+
+print("Atualmente, seus", Dollar, "dólares estão avaliados em", Reias, "Reais.")
